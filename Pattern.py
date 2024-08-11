b40c2b46c5821cd811c1897c4c0f6d2acfc3527a
agamjain01 <agam8126@gmail.com>
2024-08-12 00:11:33 +0530

    Pattern In Python
    That is My starting coding . in this file we are see this same pattern question .

---
+++ Pattern.py
@@ -0,0 +1,108 @@
+# WAP Draw pattern *
+
+# for i in range(4):
+#     for j in range(4):
+#         print(" * ",end="")
+
+    # print()
+ # *  *  *  *
+ # *  *  *  *
+ # *  *  *  *
+ # *  *  *  *
+
+
+# for i in range(4):
+#     for j in range(i+1):
+#         print(" * ",end="")
+#     print()
+#
+#  *
+#  *  *
+#  *  *  *
+#  *  *  *  *
+
+# for i in range(4):
+#     for j in range(i+1):
+#         print(j+1," ",end="")
+#     print()
+#
+#
+# 1
+# 1  2
+# 1  2  3
+# 1  2  3  4
+
+
+# for i in range(4):
+#     for j in range(4-i):
+#         print(j+1," ",end="")
+#     print()
+#
+#
+# 1  2  3  4
+# 1  2  3
+# 1  2
+# 1
+
+#
+# for i in range(4):
+#     for j in range(i+1):
+#         print(i+1," ",end="")
+#
+#     print()
+#
+#
+# 1
+# 2  2
+# 3  3  3
+# 4  4  4  4
+
+
+# for i in range(4):
+#     for k in range(4-i):
+#         print(" ",end="")
+#         k-=1
+#     for j in range(i + 1):
+#         print(i + 1, " ", end="")
+#
+#     print()
+#
+#
+#     1
+#    2  2
+#   3  3  3
+#  4  4  4  4
+
+
+
+
+#
+# for i in range(5):
+#     for k in range(5-i):
+#         print(" ",end="")
+#         k-=1
+#     for j in range(i + 1):
+#         print(i + 1, " ", end="")
+#
+#     print()
+#
+# for i in range(5):
+#     for k in range(i+1):
+#         print(" ",end="")
+#         k+=1
+#     for j in range(4-i):
+#         print(4-i, " ", end="")
+#
+#     print()
+#
+#
+#
+#      1
+#     2  2
+#    3  3  3
+#   4  4  4  4
+#  5  5  5  5  5
+#  4  4  4  4
+#   3  3  3
+#    2  2
+#     1
